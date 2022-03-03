5ecacd4ac9d874541c2afe74661b841cb7fc3e59
Bidisha Rath <rathb@mymail.vcu.edu>
2022-03-02 22:27:44 -0500

    added settings page

--- app/src/main/java/com/example/squaregameconnected/MainActivity.java
+++ app/src/main/java/com/example/squaregameconnected/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    ImageView settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +25,29 @@ public class MainActivity extends AppCompatActivity {
                 openSelectionPage();
             }
         });
+
+        settingsButton = (ImageView) findViewById(R.id.setting_buttonhome);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettingsActivity();
+            }
+
+        });
     }
+
 
     public void openSelectionPage() {
         Intent intent = new Intent(this, SelectionPage.class);
         startActivity(intent);
     }
+
+    public void openSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
