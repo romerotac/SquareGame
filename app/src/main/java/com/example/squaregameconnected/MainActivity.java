3c35838028179e496837756a928ca095c3e6a90e
Bidisha Rath <rathb@mymail.vcu.edu>
2022-03-02 23:14:24 -0500

    added settings page

--- app/src/main/java/com/example/squaregameconnected/MainActivity.java
+++ app/src/main/java/com/example/squaregameconnected/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     ImageView settingsButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
